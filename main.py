f1db8992c51aa4e304c06a39a02af4f21baa91a0
Coro19 <102507061+Coro19@users.noreply.github.com>
2025-11-06 13:48:03 +0200

    Created basic GUI

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 import sys, subprocess
-select = input("1-Run face recognition\n 2-Build face encodings database\nChoose an option (1 or 2): ")
-if select == "1":
-    subprocess.run([sys.executable, "face_recognition.py"])
-elif select == "2":
-    subprocess.run([sys.executable, "build_encodings.py"])
-else:
-    print("Invalid option. Please choose 1 or 2.")
+import tkinter as tk
+root = tk.Tk()
+root.title("Face Recognition Options")
+root.geometry("500x500")
+btn_width = 40
+btn_height = 3
+extra_ipady = 8
+button1 = tk.Button(root,width = btn_width, height = btn_height,
+                    text="Run Face Recognition", font = ('Arial', 15),
+                    command=lambda: run_option("1"))
+button1.pack(pady=20, ipady = extra_ipady)
+button2 = tk.Button(root,width = btn_width, height = btn_height,
+                    text="Build Face Encodings Database", font = ('Arial',15),
+                    command=lambda: run_option("2"))
+button2.pack(pady=20, ipady = extra_ipady)
+settings_btn = tk.Button(root, text = "Settings",
+                         font = ('Arial', 15))
+settings_btn.place(x = 400, y = 400, height = 50, width = 80)
+close_btn = tk.Button(root, text = "Close", fg = "red",
+                      font = ('Arial', 15),
+                      command=root.destroy)
+close_btn.place(x = 30, y = 400, height = 50, width = 80)
+def run_option(option):
+    root.destroy()
+    if option == "1":
+        subprocess.run([sys.executable, "face_recognition.py"])
+    elif option == "2":
+        subprocess.run([sys.executable, "build_encodings.py"])
+root.mainloop()
